0272105b958ec1bece34d00544202e96cbb98d52
ChiaHung <a0926223478@hotmail.com>
2022-06-01 12:38:44 +0900

    feat:add inex.handlebars

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
 // console.log(app.listen(port))
+app.engine('handlebars', engine({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.render('index')
 })
 
 app.listen(port, () => {
